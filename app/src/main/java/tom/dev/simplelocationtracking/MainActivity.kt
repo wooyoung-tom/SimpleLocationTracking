eb0a060809e7e2765a165a6d942778fe953a7c72
wooyoung-tom <djdj7087@gmail.com>
2021-04-13 11:01:35 +0900

    first commit

---
+++ app/src/main/java/tom/dev/simplelocationtracking/MainActivity.kt
@@ -0,0 +1,18 @@
+package tom.dev.simplelocationtracking
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import tom.dev.simplelocationtracking.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
