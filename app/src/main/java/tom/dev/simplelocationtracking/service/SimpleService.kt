c6a150d80b7a5ed4371e93f8a5fa5d06e35deea4
wooyoung-tom <djdj7087@gmail.com>
2021-04-13 15:23:59 +0900

    Simple Example Service

---
+++ app/src/main/java/tom/dev/simplelocationtracking/service/SimpleService.kt
@@ -0,0 +1,77 @@
+package tom.dev.simplelocationtracking.service
+
+import android.app.Service
+import android.content.Intent
+import android.os.*
+import android.widget.Toast
+
+/**
+ * Simple Example Service
+ */
+class SimpleService : Service() {
+
+    private var serviceLooper: Looper? = null
+    private var serviceHandler: ServiceHandler? = null
+
+    // Thread 로 부터 message 받아오는 Handler
+    private inner class ServiceHandler(looper: Looper) : Handler(looper) {
+
+        override fun handleMessage(msg: Message) {
+            // 보통 여기서 작업을 수행한다. (ex. 파일 다운로드)
+            // 이 샘플에서는 5초 sleep 을 수행한다.
+            try {
+                Thread.sleep(5000)
+            } catch (e: InterruptedException) {
+                // interrupt restore
+                Thread.currentThread().interrupt()
+            }
+
+            /**
+             * startId(= msg.arg1)를 통해서 service 종료
+             * @see [onStartCommand] startId 를 msg.arg1 로 전달
+             */
+            stopSelf(msg.arg1)
+        }
+    }
+
+    override fun onCreate() {
+        /**
+         * service 동작을 위한 thread 를 시작하는 부분이다.
+         * service 는 보통 process 의 main thread 에서 동작하기 때문에
+         * 앱이 멈추지 않기 위해서는 따로 thread 를 만들어 주어야 한다.
+         * 또한 CPU-intensive 작업이 UI를 방해하지 않도록
+         * [Process.THREAD_PRIORITY_BACKGROUND] 설정해주어야 한다.
+         */
+        HandlerThread("ServiceStartArgs", Process.THREAD_PRIORITY_BACKGROUND).apply {
+            start()
+
+            // HandlerThread 의 Looper 를 가져와서 custom 한 Handler 에서 사용하도록 한다.
+            serviceLooper = looper
+            serviceHandler = ServiceHandler(looper)
+        }
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Toast.makeText(this, "service starting", Toast.LENGTH_SHORT).show()
+
+        /**
+         * 각각의 시작 요청마다, job 을 시작하라고 message 를 보내면서 start ID 를 전달한다.
+         * 그로인해 job 이 종료될 때 어떤 요청을 멈춰야 하는지 알 수 있다.
+         */
+        serviceHandler?.obtainMessage()?.also { msg ->
+            msg.arg1 = startId
+            serviceHandler?.sendMessage(msg)
+        }
+
+        return START_STICKY
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        // Binding 사용하지 않을 것이기 때문에 null 반환
+        return null
+    }
+
+    override fun onDestroy() {
+        Toast.makeText(this, "service done", Toast.LENGTH_SHORT).show()
+    }
+}
--- app/src/main/java/tom/dev/simplelocationtracking/ui/main/MainFragment.kt
+++ app/src/main/java/tom/dev/simplelocationtracking/ui/main/MainFragment.kt
@@ -1,12 +1,17 @@
 package tom.dev.simplelocationtracking.ui.main
 
+import android.content.Intent
+import android.os.Build
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import tom.dev.simplelocationtracking.R
+import tom.dev.simplelocationtracking.service.SimpleService
 
 class MainFragment : Fragment() {
 
@@ -16,15 +21,26 @@ class MainFragment : Fragment() {
 
     private lateinit var viewModel: MainViewModel
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         return inflater.inflate(R.layout.main_fragment, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val serviceButton = view.findViewById<Button>(R.id.btn_start_service)
+
+        serviceButton.setOnClickListener {
+            Intent(requireContext(), SimpleService::class.java).also {
+                requireActivity().startService(it)
+            }
+        }
     }
 
 }
