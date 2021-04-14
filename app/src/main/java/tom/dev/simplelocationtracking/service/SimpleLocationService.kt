820ba95e65b7b850290f57153af40d378632531a
wooyoung-tom <djdj7087@gmail.com>
2021-04-14 10:14:11 +0900

    location 추가 중

--- app/src/main/java/tom/dev/simplelocationtracking/service/SimpleLocationService.kt
+++ app/src/main/java/tom/dev/simplelocationtracking/service/SimpleLocationService.kt
@@ -1,22 +1,52 @@
 package tom.dev.simplelocationtracking.service
 
+import android.Manifest.permission.*
 import android.app.Service
 import android.content.Intent
+import android.content.pm.PackageManager.PERMISSION_GRANTED
 import android.os.*
+import android.util.Log
 import android.widget.Toast
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
 
 class SimpleLocationService : Service() {
 
     private var serviceLooper: Looper? = null
     private var serviceHandler: LocationServiceHandler? = null
 
-    private inner class LocationServiceHandler(looper: Looper) : Handler(looper) {
-        override fun handleMessage(msg: Message) {
+    // Location Client
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
+    private inner class LocationServiceHandler(looper: Looper) : Handler(looper) {
+        private val locationRequest = LocationRequest.create().apply {
+
+        }
+
+        private val locationCallback = object : LocationCallback() {
+            override fun onLocationResult(location: LocationResult) {
+                Log.d("Location Callback", "$location")
+            }
+        }
+
+        override fun handleMessage(msg: Message) {
+            if (checkSelfPermission(ACCESS_COARSE_LOCATION) == PERMISSION_GRANTED
+                && checkSelfPermission(ACCESS_FINE_LOCATION) == PERMISSION_GRANTED
+                && checkSelfPermission(ACCESS_BACKGROUND_LOCATION) == PERMISSION_GRANTED
+            ) {
+                fusedLocationProviderClient.requestLocationUpdates(
+                    locationRequest, locationCallback, looper
+                )
+            }
         }
     }
 
     override fun onCreate() {
+        // Location Client 초기화
+        fusedLocationProviderClient = FusedLocationProviderClient(this)
+
         HandlerThread("LocationArgs", Process.THREAD_PRIORITY_BACKGROUND).apply {
             start()
 
